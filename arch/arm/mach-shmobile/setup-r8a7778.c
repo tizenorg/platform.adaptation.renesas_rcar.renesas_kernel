d2f7a8c47d32fba409d1deb5ad55bcd035924caa
Geert Uytterhoeven <geert+renesas@glider.be>
2014-08-13 13:45:00 +0200

    ARM: shmobile: r8a7778: Add missing call to shmobile_init_late()
    Add the missing call to shmobile_init_late() to r8a7778_init_late().
    This should make sure Suspend-to-RAM and CPUIdle are setup as expected
    on bockw.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    (cherry picked from commit 54a06dde40f6a01e323d49390d78b8c9f1dfacc0)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/setup-r8a7778.c
+++ arch/arm/mach-shmobile/setup-r8a7778.c
@@ -521,6 +521,7 @@ void __init r8a7778_add_standard_devices(void)
 
 void __init r8a7778_init_late(void)
 {
+	shmobile_init_late();
 	platform_device_register_full(&ehci_info);
 	platform_device_register_full(&ohci_info);
 }
